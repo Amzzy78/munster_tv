94b6ee3c950d0ebe8521c6cbe785eee96e3c22b7
Amzzy78 <murphy.annmarie78@gmail.com>
2021-09-20 19:58:55 +0000

    Troubleshoot max_row type as throwing syntax error

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import openpyxl
 import gspread
 from google.oauth2.service_account import Credentials
+
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,9 +15,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 SHEET = GSPREAD_CLIENT.open('munster-tv')
-
+# Balance sheet
 balance = SHEET.worksheet('balance')
 
 data = balance.get_all_values()
 
-print(data)
+#print(data)
+
+# Update Inventory sheet
+inv_file = openpyxl.load_workbook('munster-tv')
+# Variable to work on inventory sheet
+product_list = inv_file['inventory'] 
+
+# Calculate how many products per supplier
+product_per_supplier = {} # Dict
+# Loop through the rows
+for product_row in  int(product_list.max_row)
+
+print(product_list.max_row)
